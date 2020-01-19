7bce06b6bbd9984db125690cac44dc5fba9dc59a
Aaron Minyard <amnyrrd@gmail.com>
2020-01-19 00:48:18 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -96,12 +96,13 @@ def reduce_to_all_true(source_array)
 end
 
 def reduce_to_any_true(source_array)
-  new_arr = []
-  source_array.each do |i|
-    if i == true
+  i = 0
+  while i < source_array.length do 
+    if source_array[i] == true
       return true
-    else
-      return false
     end
+    i += 1
   end
+  return false
+end
 end
