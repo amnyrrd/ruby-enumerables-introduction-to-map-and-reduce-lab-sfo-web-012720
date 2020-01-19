eda9754d5e6a0f852e34489c78d92a636bbb1e58
Aaron Minyard <amnyrrd@gmail.com>
2020-01-19 00:46:59 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -89,8 +89,8 @@ def reduce_to_all_true(source_array)
   while i < source_array.length do
     if source_array[i] == false
       return false
-      i += 1
     end
+    i += 1
   end
     return true
 end
